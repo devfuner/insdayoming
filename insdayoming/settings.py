359fe86b5e174482e7ad6b1ead4622920df4ce01
devfuner <devfuner@google.com>
2018-07-31 00:02:58 +0900

    커뮤니티 베이스 파일 추가 및 적용

--- insdayoming/settings.py
+++ insdayoming/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'insdayoming.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'community/templates/community')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
