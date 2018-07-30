4e21722964487bfb08f9bee0a779f37ba15c71c4
devfuner <devfuner@google.com>
2018-07-30 20:33:37 +0900

    write 입력 폼 추가

--- community/views.py
+++ community/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from community.forms import *
 
 
 # Create your views here.
 def write(request):
-    return render(request, 'write.html')
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = Form()
+
+    return render(request, 'write.html', {'form': form})
