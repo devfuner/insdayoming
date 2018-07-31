d00b1f97f15653e07b39b8f6f02aeca61ff9e236
devfuner <devfuner@google.com>
2018-07-31 10:03:54 +0900

    업로드 이미지 서비스 추가
    - 업로드한 이미지를 url로 접근할 수 있도록 설정 추가

--- instagram/models.py
+++ instagram/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     name = models.CharField(max_length=50)
-    photo = models.ImageField(blank=False)
+    photo = models.ImageField(blank=False, upload_to='instagram/%Y/%m/%d')
     contents = models.TextField()
     cdate = models.DateTimeField(auto_now_add=True)
